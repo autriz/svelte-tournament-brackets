242a9d67a74431d3a8305e9a67cd7f9f47f30efc
autriz <explayinformer8519@gmail.com>
2024-09-12 12:28:31 +0400

    Update index.ts

--- src/lib/brackets/index.ts
+++ src/lib/brackets/index.ts
@@ -1,5 +1,3 @@
-import { DoubleEliminationBracket } from "./double-elimination/index.js";
-import { SingleEliminationBracket } from "./single-elimination/index.js";
-import { RoundRobinBracket } from "./round-robin/index.js";
-
-export { DoubleEliminationBracket, SingleEliminationBracket, RoundRobinBracket };
+export { DoubleEliminationBracket } from "./double-elimination/index.js";
+export { SingleEliminationBracket } from "./single-elimination/index.js";
+export { RoundRobinBracket } from "./round-robin/index.js";
