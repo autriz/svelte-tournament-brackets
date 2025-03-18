51b384e0f42ab944dadbfa2b7c24e7d058618e24
autriz <explayinformer8519@gmail.com>
2025-03-18 11:42:53 +0300

    Fixed icon animation

--- src/components/ThemeToggleButton.svelte
+++ src/components/ThemeToggleButton.svelte
@@ -16,15 +16,19 @@
 		rounded-md size-10", className)}
 	on:click={toggleMode}
 >
-	<div class="absolute inline-flex h-full w-full justify-center items-center">
-		{#if $mode === "dark"}
-			<div transition:scale={{ duration: 100, start: 0.7 }}>
-				<Moon class="size-5 fill-[#24292f] dark:fill-[#fff]" />
-			</div>
-		{:else}
-			<div transition:scale={{ duration: 100, start: 0.7 }}>
-				<Sun class="size-5 fill-[#24292f] dark:fill-[#fff]" />
-			</div>
-		{/if}
-	</div>
+	{#if $mode === "dark"}
+		<div 
+			class="absolute inline-flex h-full w-full items-center justify-center"
+			transition:scale={{ duration: 200, delay: 50, start: 0.7 }}
+		>
+			<Moon class="size-5 fill-[#24292f] dark:fill-[#fff]" />
+		</div>
+	{:else}
+		<div 
+			class="absolute inline-flex h-full w-full items-center justify-center"
+			transition:scale={{ duration: 200, delay: 50, start: 0.7 }}
+		>
+			<Sun class="size-5 fill-[#24292f] dark:fill-[#fff]" />
+		</div>
+	{/if}
 </button>
