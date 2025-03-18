eeafa671c398f3dc7ff022cfd493c1f05070f7f8
autriz <explayinformer8519@gmail.com>
2025-03-18 11:43:22 +0300

    Added icon animation to copy code button

--- src/components/CodeBlock.svelte
+++ src/components/CodeBlock.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import clsx from "clsx";
-	import { Clipboard } from "lucide-svelte";
+	import { AlertCircle, Check, Clipboard } from "lucide-svelte";
 	import type { HighlighterCore } from "shiki";
+	import { scale } from "svelte/transition";
+	import ThemeToggleButton from "./ThemeToggleButton.svelte";
 
 	export let highlighter: HighlighterCore;
 	export let code: string;
@@ -17,22 +19,59 @@
 		},
 	});
 
+	let copyTimeout: number | undefined;
+	let copyStatus: "copied" | "failed" | null = null;
+
 	function copyCode() {
-		navigator.clipboard.writeText(code);
+		if (navigator.clipboard) {
+			navigator.clipboard.writeText(code);
+			copyStatus = "copied";
+		} else {
+			copyStatus = "failed";
+		}
+
+		clearTimeout(copyTimeout);
+
+		copyTimeout = setTimeout(() => {
+			copyStatus = null;
+		}, 2500);
 	}
 </script>
 
 <div class={clsx("group relative overflow-x-auto rounded-lg", className)}>
-	<button
-		class="absolute right-[12px] top-[12px] rounded-lg border border-neutral-400
-		bg-neutral-100 p-1.5 opacity-0
-		transition-all hover:bg-neutral-50 group-hover:opacity-100
-		dark:border-neutral-700 dark:bg-neutral-800 dark:hover:bg-neutral-900"
-		title="Copy code"
-		on:click={copyCode}
-	>
-		<Clipboard class="size-5 text-neutral-500"></Clipboard>
-	</button>
+	<div class="absolute right-[12px] top-[12px] inline-flex size-8">
+		<button
+			class="relative rounded-lg border border-neutral-400
+			bg-neutral-100 opacity-0 size-8 inline-flex items-center justify-center
+			transition-all hover:bg-neutral-50 group-hover:opacity-100
+			dark:border-neutral-700 dark:bg-neutral-800 dark:hover:bg-neutral-900"
+			title="Copy code"
+			on:click={copyCode}
+		>
+			{#if copyStatus === "copied"}
+				<div 
+					class="absolute inline-flex h-full w-full justify-center items-center"
+					transition:scale={{ duration: 100, delay: 50, start: 0.7 }}
+				>
+					<Check class="size-5 text-green-600"></Check>
+				</div>
+			{:else if copyStatus === "failed"}
+				<div 
+					class="absolute inline-flex h-full w-full justify-center items-center"
+					transition:scale={{ duration: 100, delay: 50, start: 0.7 }}
+				>
+					<AlertCircle class="size-5 text-red-500"></AlertCircle>
+				</div>
+			{:else}
+				<div 
+					class="absolute inline-flex h-full w-full justify-center items-center"
+					transition:scale={{ duration: 100, delay: 50, start: 0.7 }}
+				>
+					<Clipboard class="size-5 text-neutral-500"></Clipboard>
+				</div>
+			{/if}
+		</button>
+	</div>
 	{@html html}
 </div>
 
