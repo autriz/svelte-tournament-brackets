fbc75dd1ef13d7646d6cbb54c005344c94332e8b
autriz <explayinformer8519@gmail.com>
2025-04-03 12:43:16 +0400

    adjusted styles for mobile support

--- src/docs/components/FooterNavigation.svelte
+++ src/docs/components/FooterNavigation.svelte
@@ -11,23 +11,37 @@
 	} | undefined = undefined;
 </script>
 
-<div class="border-t border-dashed mt-8 mb-4 flex justify-between py-6 px-8 border-neutral-300 dark:border-neutral-700">
+<div
+	class="mb-4 mt-8 flex flex-col justify-between gap-4 border-t border-dashed border-neutral-300 px-8 py-6 dark:border-neutral-700 sm:flex-row sm:gap-0"
+>
 	{#if previous}
-		<a class="items-end flex flex-row-reverse mr-auto group" href={previous.href}>
+		<a
+			class="group flex flex-row-reverse items-end justify-center sm:mr-auto sm:justify-normal"
+			href={previous.href}
+		>
 			<div class="flex flex-col text-start">
-				<span class="text-[13px] text-muted-foreground">Previous page</span>
+				<span class="text-[13px] text-muted-foreground"
+					>Previous page</span
+				>
 				<span class="text-[14px] font-medium">{previous.title}</span>
 			</div>
-			<ChevronLeft class="mr-1 text-muted-foreground group-hover:text-foreground group-hover:-translate-x-0.5 transition-all" />
+			<ChevronLeft
+				class="mr-1 text-muted-foreground transition-all group-hover:-translate-x-0.5 group-hover:text-foreground"
+			/>
 		</a>
 	{/if}
 	{#if next}
-		<a class="items-end flex ml-auto group" href={next.href}>
+		<a
+			class="group flex items-end justify-center sm:ml-auto sm:justify-normal"
+			href={next.href}
+		>
 			<div class="flex flex-col text-end">
 				<span class="text-[13px] text-muted-foreground">Next page</span>
 				<span class="text-[14px] font-medium">{next.title}</span>
 			</div>
-			<ChevronRight class="ml-1 text-muted-foreground group-hover:text-foreground group-hover:translate-x-0.5 transition-all" />
+			<ChevronRight
+				class="ml-1 text-muted-foreground transition-all group-hover:translate-x-0.5 group-hover:text-foreground"
+			/>
 		</a>
 	{/if}
 </div>
