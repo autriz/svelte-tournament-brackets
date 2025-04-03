dc23200d1ed69a3cc8bc8387fdc05c7d6a82bfca
autriz <explayinformer8519@gmail.com>
2025-04-03 12:47:53 +0400

    forgot to remove round robin export

--- src/lib/brackets/index.ts
+++ src/lib/brackets/index.ts
@@ -1,5 +1,4 @@
 export { DoubleEliminationBracket } from "./double-elimination/index.js";
 export { SingleEliminationBracket } from "./single-elimination/index.js";
-export { RoundRobinBracket } from "./round-robin/index.js";
 
 export * from "./types.js";
